6d46afa0938a2798f1f49cfb13daf5ec6666e086
Anuj Soni <anujsoni08@gmail.com>
2020-04-29 21:38:32 +0530

    formatted the code

--- src/components/LandingPageComponent/landingPage.js
+++ src/components/LandingPageComponent/landingPage.js
@@ -7,8 +7,7 @@ class LandingPage extends Component {
       <div className="landing-page-div">
         <div className="justify-content-center align-items-center">
           <h1 className="greet-text">
-            Hi, I am{" "}
-            <span style={{ color: "rgb(26, 115, 232)" }}> Anuj</span>
+            Hi, I am <span style={{ color: "rgb(26, 115, 232)" }}> Anuj</span>
           </h1>
           <div className="profile-text">
             <p>Frontend Developer</p>
@@ -23,54 +22,64 @@ class LandingPage extends Component {
           </p>
           <div className="flex text-md-center mt-2">
             <img
-              src="https://image.flaticon.com/icons/svg/888/888859.svg"
-              className="m-2"
-              style={{ maxWidth: "8vh" }}
-            />
-            <img
-              src="https://image.flaticon.com/icons/svg/888/888847.svg"
-              className="m-2"
-              style={{ maxWidth: "8vh" }}
-            />
-            <img
-              src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/267_Python_logo-512.png"
-              className="m-2"
-              style={{ maxWidth: "8vh" }}
-            />
-            <img
-              src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
-              className="m-2"
-              style={{ maxWidth: "8vh" }}
-            />
-            <img
               src="https://image.flaticon.com/icons/svg/919/919832.svg"
               className="m-2"
               style={{ maxWidth: "8vh" }}
+              alt="typescript"
             />
             <img
               src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/187_Js_logo_logos-512.png"
               className="m-2"
               style={{ maxWidth: "8vh" }}
-            />
-            <img
-              src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/233_Node_Js_logo-512.png"
-              className="m-2"
-              style={{ maxWidth: "8vh" }}
-            />
-            <img
-              src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/288_Sass_logo-512.png"
-              className="m-2"
-              style={{ maxWidth: "8vh" }}
-            />
-            <img
-              src="https://cdn4.iconfinder.com/data/icons/logos-3/512/mongodb-2-512.png"
-              className="m-2"
-              style={{ maxWidth: "8vh" }}
+              alt="javascript"
             />
             <img
               src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/21_Angular_logo_logos-512.png"
               className="m-2"
               style={{ maxWidth: "8vh" }}
+              alt="angular"
+            />
+            <img
+              src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+              className="m-2"
+              style={{ maxWidth: "8vh" }}
+              alt="react"
+            />
+            <img
+              src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/233_Node_Js_logo-512.png"
+              className="m-2"
+              style={{ maxWidth: "8vh" }}
+              alt="nodejs"
+            />
+            <img
+              src="https://cdn4.iconfinder.com/data/icons/logos-3/512/mongodb-2-512.png"
+              className="m-2"
+              style={{ maxWidth: "8vh" }}
+              alt="mongodb"
+            />
+            <img
+              src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/267_Python_logo-512.png"
+              className="m-2"
+              style={{ maxWidth: "8vh" }}
+              alt="python"
+            />
+            <img
+              src="https://image.flaticon.com/icons/svg/888/888859.svg"
+              className="m-2"
+              style={{ maxWidth: "8vh" }}
+              alt="html5"
+            />
+            <img
+              src="https://image.flaticon.com/icons/svg/888/888847.svg"
+              className="m-2"
+              style={{ maxWidth: "8vh" }}
+              alt="css3"
+            />
+            <img
+              src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/288_Sass_logo-512.png"
+              className="m-2"
+              style={{ maxWidth: "8vh" }}
+              alt="sass"
             />
           </div>
         </div>
